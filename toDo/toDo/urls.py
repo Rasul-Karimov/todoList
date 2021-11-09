bcf3037fac378dc82a78ed490851131fc01aec99
Rasulbek <rasulbekkari008@gmail.com>
2021-11-09 09:54:10 +0600

    added todo modesl and in admin

--- toDo/toDo/urls.py
+++ toDo/toDo/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage
-
+from main.views import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', homepage)
+    path('', homepage),
+    path('test', test)
 ]
